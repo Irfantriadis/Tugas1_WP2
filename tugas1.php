ef6daedff8fcbb30ab42d01b5473ddb94b95ad17
Irfan Triadi Saputra <72369571+Irfantriadis@users.noreply.github.com>
2022-03-02 12:08:29 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,12 @@
+<?php
+
+ $nama = "Irfan Triadi Saputra"; 
+ $nim = "20090017";  
+ $kelas = "4B";
+
+ echo "Halo! Perkenalkan Saya,\n";
+ echo "Nama     : $nama\n";
+ echo "Nim      : $nim\n";
+ echo "Kelas    : $kelas\n";
+
+?>
